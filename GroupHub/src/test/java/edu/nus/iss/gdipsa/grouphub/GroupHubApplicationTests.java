a519278fb89d80c745468acc0a260531582e6acc
Shuai Hao <e1221805@u.nus.edu>
2024-01-26 22:33:55 +0800

    2024/1/31

---
+++ GroupHub/src/test/java/edu/nus/iss/gdipsa/grouphub/GroupHubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.gdipsa.grouphub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
