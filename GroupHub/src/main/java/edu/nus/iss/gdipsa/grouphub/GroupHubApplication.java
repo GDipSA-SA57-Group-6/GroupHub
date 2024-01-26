c4f708bdaac9e999607b04d4738ef054de75065b
Shuai Hao <e1221805@u.nus.edu>
2024-01-26 22:33:55 +0800

    2024/1/26

---
+++ GroupHub/src/main/java/edu/nus/iss/gdipsa/grouphub/GroupHubApplication.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.gdipsa.grouphub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupHubApplication.class, args);
+    }
+
+}
